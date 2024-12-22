df40ae8097b7a6acd5b2a3156e094b0f7f54de69
mysorf-9239 <uydanh214@gmail.com>
2024-12-22 15:18:50 +0700

    feat: cors

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,23 @@
 import express, { Application } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import userRoutes from './routes/user.routes';
-
+import routes from './routes';
 
 const app: Application = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors());
+
+// app.use(
+//     cors({
+//         origin: 'http://localhost:5173',
+//         credentials: true
+//     })
+// );
 
 // Middleware
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/users', userRoutes);
+app.use('/api', routes);
 
 export default app;
