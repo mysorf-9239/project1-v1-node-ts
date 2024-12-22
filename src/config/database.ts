b1f11098978297ec579958ecd84aabc439aaddfc
mysorf-9239 <uydanh214@gmail.com>
2024-12-22 15:44:35 +0700

    feat: railway database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,13 +1,22 @@
 import { Sequelize } from 'sequelize';
 
-const username = process.env.DATABASE_USERNAME || "root";
-const password = process.env.DATABASE_PASSWORD || "password";
-const database_name = process.env.DATABASE_NAME || "project1";
+const DATABASE_URL = process.env.MYSQL_URL;
 
-const sequelize = new Sequelize(database_name, username, password, {
-    host: process.env.DATABASE_HOST,
-    dialect: 'mysql',
-    logging: false,
-});
+const sequelize = DATABASE_URL
+    ? new Sequelize(DATABASE_URL, {
+        dialect: 'mysql',
+        logging: false,
+    })
+    : new Sequelize(
+        process.env.MYSQLDATABASE || "project1",
+        process.env.MYSQL_USER || "root",
+        process.env.MYSQL_PASSWORD || "password",
+        {
+            host: process.env.MYSQLHOST || "localhost",
+            dialect: 'mysql',
+            port: +(process.env.MYSQLPORT ?? 3306),
+            logging: false,
+        }
+    );
 
 export default sequelize;
